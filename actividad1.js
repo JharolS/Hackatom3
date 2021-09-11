fb1d33e579449f9f9edea89bcd5cbef3887566f1
unknown <sv73107535@idat.edu.pe>
2021-09-11 12:14:26 -0500

    first commit

--- actividad1.js
+++ actividad1.js
@@ -1,3 +1,9 @@
-let i = 5;
-let a= 6
-console.log(i,a)
+let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+function aclean(list) {
+    const filter1 = list.findIndex((element) => ['nap'].includes(element))
+    const filter2 = list.findIndex((element) => ['teachers'].includes(element))
+    const filter3 = list.findIndex((element) => ['ear'].includes(element))
+    console.log (list[filter1],list[filter2],list[filter3])
+    console.log (list[filter1])
+}
+aclean(arr);
